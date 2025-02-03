1c298192b15c423301e09bea57608ffd253220f5
Lara <lara.salman01@hotmail.com>
2025-02-03 19:18:11 +0100

    create basis for footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,12 @@
+<footer>
+    <a href="/">MovieScout<!--Movie scout logo here--></a>
+    <a href="/">Home</a>
+    <a href="/">Genre</a>
+    <a href="/">Watchlist</a>
+    <a href="/">Sign in</a>
+    <!--Instagram logo-->
+    <!--Facebook logo-->
+    <!--X logo-->
+    <!--Tiktok logo-->
+    <!--Copyright mark/text--> 
+</footer>
