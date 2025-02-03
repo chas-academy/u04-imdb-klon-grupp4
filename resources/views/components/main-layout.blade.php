5f006b1183a6bddfb740256d5a2bfe746fe24cdf
Fredric Laestander <fredriclaestander@gmail.com>
2025-02-03 21:25:13 +0100

    create base layout for main

--- resources/views/components/main-layout.blade.php
+++ resources/views/components/main-layout.blade.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="font-outfit bg-neutral-800 text-base text-neutral-100">
     <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Featured</title>
+        <title>Movie Scout</title>
         <link rel="icon" href="{{ asset("images/logomark.svg") }}" />
+        @vite(["resources/css/app.css", "resources/js/app.js"])
     </head>
     <body>
         <x-navbar />
