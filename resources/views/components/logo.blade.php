f6f8891fbaec725579704f988318d2bfabb0a63d
Fredric Laestander <fredriclaestander@gmail.com>
2025-02-04 00:32:20 +0100

    remove background

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -3,9 +3,7 @@
     "responsive" => false,
 ])
 
-<div
-    class="text-sun-400 fixed flex w-full gap-2 bg-neutral-900 text-2xl font-bold"
->
+<div class="text-sun-400 fixed flex w-full gap-2 text-2xl font-bold">
     <img src="{{ asset("images/logomark.svg") }}" alt="Movie Scout" />
 
     @if ($showText)
