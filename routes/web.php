f627a348eae401b59a9fd360cbba82e46a0ff979
SRsoz <sozyar.rauf@chasacademy.se>
2025-02-04 16:30:58 +0100

    fixed conflicts

--- routes/web.php
+++ routes/web.php
@@ -6,68 +6,68 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('Homepage');
 });
-Route::get('/admin_delete_titles', function () {
-    return view('Delete titles');
+Route::get('/admin-delete-titles', function () {
+    return view('admin-delete-titles');
 });
-Route::get('/admin_delete_ratings', function () {
-    return view('Delete ratings');
+Route::get('/admin-delete-rating', function () {
+    return view('admin-delete-rating');
 });
-Route::get('/admin_manage_user', function () {
-    return view('Manage user');
+Route::get('/admin-manage-user', function () {
+    return view('admin-manage-user');
 });
-Route::get('/admin_settings', function () {
-    return view('Admin settings');
+Route::get('/admin-settings', function () {
+    return view('admin-settings');
 });
 Route::get('/admin', function () {
-    return view('Admin');
+    return view('admin');
 });
-Route::get('/change_user_info', function () {
-    return view('Change information');
+Route::get('/change-user-info', function () {
+    return view('change-user-info');
 });
-Route::get('/create_account', function () {
-    return view('Create account');
+Route::get('/create-account', function () {
+    return view('create-account');
 });
-Route::get('/create_list', function () {
-    return view('Create new list');
+Route::get('/create-list', function () {
+    return view('create-list');
 });
-Route::get('/delete_account', function () {
-    return view('Delete your account');
+Route::get('/delete-account', function () {
+    return view('delete-account');
 });
 Route::get('/genres', function () {
-    return view('Genres');
+    return view('genres');
 });
-Route::get('/log_out', function () {
-    return view('Log out from account');
+Route::get('/log-out', function () {
+    return view('log-out');
 });
 Route::get('/movies', function () {
-    return view('Movies');
+    return view('movies');
 });
 Route::get('/ratings', function () {
-    return view('Ratings');
+    return view('ratings');
 });
-Route::get('/recently_viewed', function () {
-    return view('Recently viewed');
+Route::get('/recently-viewed', function () {
+    return view('recently-viewed');
 });
-Route::get('/sign_in', function () {
-    return view('Sign in to your account');
+Route::get('/sign-in', function () {
+    return view('sign-in');
 });
-Route::get('/signed_in', function () {
-    return view('Signed in');
+Route::get('/signed-in', function () {
+    return view('signed-in');
 });
-Route::get('/specific_movie', function () {
-    return view('Specific movies');
+Route::get('/specific-movie', function () {
+    return view('specific-movie');
 });
-Route::get('/top_titles', function () {
-    return view('Top titles');
+Route::get('/top-titles', function () {
+    return view('top-titles');
 });
-Route::get('/user_settings', function () {
-    return view('User settings');
+Route::get('/user-settings', function () {
+    return view('user-settings');
 });
 Route::get('/user', function () {
-    return view('User');
+    return view('user');
 });
 Route::get('/watchlist', function () {
-    return view('+Watchlist');
+    return view('watchlist');
 });
 
 Route::get('/dashboard', function () {
