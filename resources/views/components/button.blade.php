7e5ccd3d7e916228f04c4490aeddf5c8cab06818
FredricLaestander <fredriclaestander@gmail.com>
2025-02-04 18:29:18 +0100

    add icon slot in button

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -8,8 +8,11 @@
         "bg-sun-400 hover:bg-sun-500" => $variant === "primary",
         "bg-neutral-900 hover:bg-neutral-700" => $variant === "secondary",
         "w-full" => $wide,
-        "gap-2 rounded-full px-4 py-2 font-bold transition",
+        "item-center flex gap-2 rounded-full px-4 py-2 font-bold transition",
     ])
 >
+    <div class="size-6">
+        {{ $icon }}
+    </div>
     {{ $slot }}
 </button>
