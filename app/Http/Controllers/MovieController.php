d2eace8d02180ff3f0301c92b91935baa02ae597
elFarfar <alex.ohlin96@gmail.com>
2025-02-05 11:11:46 +0100

    change to correct letter-case

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -1,21 +1,21 @@
 <?php
-namespace App\Http\Controllers;
+namespace app\Http\Controllers;
 
-use App\Models\Movie;
-use App\Models\Genre;
+use app\Models\movie;
+use app\Models\genre;
 use Illuminate\Http\Request;
 
-class MovieController extends Controller
+class movieController extends Controller
 {
     // Show all movies in a specific genre
-    public function index(Genre $genre)
+    public function index(genre $genre)
     {
         $movies = $genre->movies; // Fetch movies that belong to this genre
         return view('movies.index', compact('movies', 'genre'));
     }
 
     // Show a single movie's details
-    public function show(Movie $movie)
+    public function show(movie $movie)
     {
         return view('movies.show', compact('movie'));
     }
