2fb0d161812231e89753c6c7d674e0514ebf71b3
Lara <lara.salman01@hotmail.com>
2025-02-07 18:15:35 +0100

    create input fields

---
+++ resources/views/components/input-field-desktop.blade.php
@@ -0,0 +1,29 @@
+@props([
+    "type" => "text",
+    "id" => null,
+    "placeholder" => "",
+    "variant" => "primary"
+])
+
+<form class="flex flex-col gap-2 w-[608px]">
+    <label for="{{ $id }}" class="block text-base text-neutral-100 font-bold">
+        {{ $slot }}
+    </label>
+
+    @if ($variant === "secondary")
+        <textarea
+            id="{{ $id }}"
+            name="{{ $id }}"
+            placeholder="{{ $placeholder }}"
+            class="p-2  h-16 rounded-lg bg-neutral-600 placeholder:text-neutral-200 text-base border-none focus:outline-none focus:ring-0 focus:border-transparent" 
+        ></textarea>
+    @else
+        <input 
+            type="{{ $type }}"
+            id="{{ $id }}"
+            name="{{ $id }}"
+            placeholder="{{ $placeholder }}"
+            class="p-2 h-10 rounded-lg bg-neutral-600 placeholder:text-neutral-200 text-base border-none focus:outline-none focus:ring-0 focus:border-transparent"
+        >
+    @endif
+</form>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,2 +1,1 @@
 <x-main-layout></x-main-layout>
-</x-input-field>
