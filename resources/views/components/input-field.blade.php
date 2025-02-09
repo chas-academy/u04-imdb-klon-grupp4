24d4f03592a4f1e7fa948ea310a2521e0bf94f75
Lara <lara.salman01@hotmail.com>
2025-02-09 02:00:26 +0100

    change style based on feedback

--- resources/views/components/input-field.blade.php
+++ resources/views/components/input-field.blade.php
@@ -5,7 +5,7 @@
     "variant" => "input",
 ])
 
-<div class="flex flex-col gap-2 w-full max-w-[20.75rem] sm:max-w-[38rem]">
+<div class="flex flex-col gap-2 w-full">
     <label for="{{ $id }}" class="block text-base text-neutral-100 font-bold">
         {{ $slot }}
     </label>
@@ -15,15 +15,14 @@
             id="{{ $id }}"
             name="{{ $id }}"
             placeholder="{{ $placeholder }}"
-            class="p-2 w-full min-h-16 rounded-lg bg-neutral-600 placeholder:text-neutral-200 text-base border-none focus:outline-none focus:ring-0 focus:border-transparent" 
+            class="p-2 w-full min-h-16 rounded-lg bg-neutral-600 placeholder:text-neutral-200 border-none focus:border-transparent" 
         ></textarea>
     @else
         <input 
-            type="{{ $type }}"
             id="{{ $id }}"
             name="{{ $id }}"
             placeholder="{{ $placeholder }}"
-            class="p-2 w-full h-10 rounded-lg bg-neutral-600 placeholder:text-neutral-200 text-base border-none focus:outline-none focus:ring-0 focus:border-transparent"
+            class="p-2 w-full rounded-lg bg-neutral-600 placeholder:text-neutral-200 border-none focus:border-transparent"
         >
     @endif
 </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,3 @@
 <x-main-layout></x-main-layout>
+<x-input-field id="name" placeholder="Placeholder">Example Name</x-input-field>
+<x-input-field id="name" placeholder="Placeholder" variant="textarea">Example Name</x-input-field>
