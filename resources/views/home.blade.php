e4f2bf0fe1d9d4acfc2d29a609b63773a41cb1d4
Lara <lara.salman01@hotmail.com>
2025-02-09 02:01:23 +0100

    remove input fields from homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,1 @@
 <x-main-layout></x-main-layout>
-<x-input-field id="name" placeholder="Placeholder">Example Name</x-input-field>
-<x-input-field id="name" placeholder="Placeholder" variant="textarea">Example Name</x-input-field>
