3132931c2a7eecd2c556879032357dbef25409fe
Lara <lara.salman01@hotmail.com>
2025-02-09 22:25:29 +0100

    create flag post component

--- resources/views/components/flag-post.blade.php
+++ resources/views/components/flag-post.blade.php
@@ -1,18 +1,3 @@
-@props ([
-    "isRed" => false,
-])
-
-<x-lucide-flag 
-    class="text-neutral-100 size-6 cursor-pointer hover:fill-sun-400 hover:text-sun-400 transition"
-    
-/>
-
-
-<script>
-    function toggleFlag() {
-        let flag
-    }
-    x-data="{ isRed: @json($isRed) }"
-    @click="if (!isRed) isRed = true"
-    :class="isRed ? 'hover:fill-sun-400 hover:text-sun-400' : 'fill-neutral-100 text-neutral-100'"
-    />
+<div class="size-6">
+    <x-lucide-flag class="cursor-pointer text-neutral-400 hover:text-neutral-100 transition"/>
+</div>
