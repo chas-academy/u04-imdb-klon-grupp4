11220a6652d94b682754c4162d0b7d94ee841e31
Lara <lara.salman01@hotmail.com>
2025-02-12 00:21:39 +0100

    style list card

--- resources/views/components/list-card.blade.php
+++ resources/views/components/list-card.blade.php
@@ -1,17 +1,19 @@
 @props ([
     "title" => "",
     "description" => "",
-    "images" => [],
+    "movies" => [],
 ])    
+
+<x-base-card class="m-4 w-full h-auto">
+    <div class="flex gap-4">
+        <x-list-cover :movies="$movies" class="w-16 h-24 object-cover" />
     
-<x-base-card>
-    {{-- movie images --}}
-    
-    <div>
-        <div class="flex justify-between">
-            <h3 class="text-lg font-bold">{{$title}}List Header</h3>
-            <x-lucide-x class="size-6 text-neutral-400 hover:text-neutral-100 cursor-pointer"></x-lucide-x>
+        <div class="flex-1">
+            <div class="flex justify-between">
+                <h3 class="text-lg font-bold">{{$title}}List Header</h3>
+                <x-lucide-x class="size-6 text-neutral-400 hover:text-neutral-100 cursor-pointer"></x-lucide-x>
+            </div>
+            <p class="line-clamp-3 sm:line-clamp-1">{{$description}}Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description </p>
         </div>
-        <p>{{$description}}Description Description Description Description Description Description Description Description Description Description Description </p>
     </div>
 </x-base-card>
