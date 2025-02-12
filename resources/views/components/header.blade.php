183770822ccd3110a109f9d478d84a1ad05eaa6f
FredricLaestander <fredriclaestander@gmail.com>
2025-02-12 14:21:20 +0100

    fix menu

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -3,7 +3,6 @@
     class="fixed z-40 flex size-20 w-full bg-neutral-900 p-4"
 >
     <x-logo showText />
-    <button @click="alert('test')">test</button>
 
     <nav class="flex items-center gap-4">
         <div class="hidden md:flex">
@@ -26,5 +25,19 @@
             <x-lucide-menu />
         </button>
     </nav>
-    {{-- <x-menu /> --}}
+
+    <div
+        x-show="open"
+        class="fixed inset-0 z-50 flex flex-col items-center justify-center gap-8 bg-neutral-900 font-bold"
+    >
+        <button @click="open = false">
+            <x-lucide-x class="absolute right-4 top-4 size-6" />
+        </button>
+
+        <a href="/">Home</a>
+        <a href="/">Genres</a>
+        <a href="/">Watchlist</a>
+        {{-- change to profile if signed in --}}
+        <a href="/">Sign in</a>
+    </div>
 </header>
