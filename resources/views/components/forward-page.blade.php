a9520660198053abf0e10ac9e4acaa72ef08e60f
Lara <lara.salman01@hotmail.com>
2025-02-13 10:55:05 +0100

    add linking pages to forward button

--- resources/views/components/forward-page.blade.php
+++ resources/views/components/forward-page.blade.php
@@ -1,6 +1,10 @@
-<button class="group flex items-center gap-3">
-    <a href="/"></a> <!--add function to link forward page-->
+@props([
+    "href" => "#",
+    "page" => "page",
+])
+
+<a href="{{ url($href)}}" class="group flex items-center gap-3">
     <div class="w-1 h-6 bg-sun-400 rounded-full"></div>
-    <div class="text-lg font-bold">Forward page</div> <!--text depending on which forward page-->
+    <div class="text-lg font-bold">{{ $page }} </div>
     <x-lucide-chevron-right class="text-neutral-100 size-6 group-hover:text-sun-400 transition"/>
-</button>
+</a>
