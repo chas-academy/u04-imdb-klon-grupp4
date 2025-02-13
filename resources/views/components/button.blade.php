cdd2a3c645893f76b6a49c2292f01f40c8cc6d94
FredricLaestander <fredriclaestander@gmail.com>
2025-02-13 17:58:38 +0100

    improve responsiveness

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -2,15 +2,18 @@
     "variant" => "primary",
     "wide" => false,
 ])
+@php
+    use Illuminate\Support\Arr;
 
-<button
-    @class([
+    $classes = Arr::toCssClasses([
         "bg-sun-400 hover:bg-sun-500" => $variant === "primary",
         "bg-neutral-900 hover:bg-neutral-700" => $variant === "secondary",
         "w-full" => $wide,
         "flex items-center justify-center gap-2 text-nowrap rounded-full px-4 py-2 font-bold transition",
-    ])
->
+    ]);
+@endphp
+
+<button {{ $attributes->class($classes) }}>
     @if (isset($icon))
         <div class="size-6">
             {{ $icon }}
