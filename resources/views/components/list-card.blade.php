40f1cdef23d7d75bd6785e1a51a7c90af72dccce
Lara <lara.salman01@hotmail.com>
2025-02-14 17:57:05 +0100

    delete unecessary text from list card

--- resources/views/components/list-card.blade.php
+++ resources/views/components/list-card.blade.php
@@ -10,10 +10,10 @@
     
         <div class="flex-1">
             <div class="flex justify-between">
-                <h3 class="text-lg font-bold">{{$title}}List Header</h3>
+                <h3 class="text-lg font-bold">{{ $title }}</h3>
                 <x-lucide-x class="size-6 text-neutral-400 hover:text-neutral-100 cursor-pointer"></x-lucide-x>
             </div>
-            <p class="line-clamp-3 sm:line-clamp-1">{{$description}}Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description Description </p>
+            <p class="line-clamp-3 sm:line-clamp-1">{{ $description }}</p>
         </div>
     </div>
 </x-base-card>
