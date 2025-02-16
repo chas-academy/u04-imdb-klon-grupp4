5f0b34b1c94c9efdb1d981f0798da927a1d64563
Fredric Laestander <fredriclaestander@gmail.com>
2025-02-16 20:49:38 +0100

    add href to subcategories

--- resources/views/components/accordion.blade.php
+++ resources/views/components/accordion.blade.php
@@ -5,20 +5,20 @@
 
 <x-base-card>
     <div x-data="{ open: false }" class="flex w-full flex-col gap-4">
-        <div class="flex justify-between">
+        <button @click="open = !open" class="flex justify-between">
             <div class="flex gap-2">
                 <div class="size-6">{{ $icon }}</div>
                 <span>{{ $category }}</span>
             </div>
 
-            <button @click="open = !open" class="size-6">
-                <x-lucide-chevron-down />
-            </button>
-        </div>
+            <x-lucide-chevron-down class="size-6" />
+        </button>
 
         <div x-show="open" class="flex flex-col pl-8">
             @foreach ($subcategories as $subcategory)
-                <span>{{ $subcategory }}</span>
+                <a href="{{ $subcategory["href"] }}">
+                    {{ $subcategory["label"] }}
+                </a>
             @endforeach
         </div>
     </div>
