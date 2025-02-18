67f5836eb6ef502ff3cc4e61bb6d434288c9ea59
Mariam Aljizawi <mariam_97@live.se>
2025-02-18 18:54:08 +0100

    fixed the models

--- app/Models/Award.php
+++ app/Models/Award.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Award extends Model
+/**class Award extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['name'];
+
+    // Relations
+    public function movies()
+    {
+        return $this->hasMany(Movie::class);
+    }
 }
+*/
--- app/Models/User.php
+++ app/Models/User.php
@@ -8,50 +8,32 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'email',
         'password',
         'is_admin',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
+
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
+   
     protected $casts = [
         'email_verified_at' => 'datetime',
         'is_admin' => 'boolean',
     ];
-
-    /**
-     * Define the many-to-many relationship with Movie.
-     */
-    /**public function movies()
+  
+    public function movies()
     {
         return $this->belongsToMany(Movie::class, 'user_movie', 'user_id', 'movie_id')->withTimestamps();
-    }*/
-
-    /**
-     * Define the many-to-many relationship with MovieList.
-     */
-    
+    }
+    public function movieLists()
+    {
+        return $this->belongsToMany(MovieList::class, 'lists', 'user_id', 'list_id')->withTimestamps();
+    }
 }
