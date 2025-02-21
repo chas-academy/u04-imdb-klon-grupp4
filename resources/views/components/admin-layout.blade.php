684fc9d71c9100ad994e77c1c984c66f568f01da
Mariam Aljizawi <mariam_97@live.se>
2025-02-21 23:29:11 +0100

    fix admin layout

--- resources/views/components/admin-layout.blade.php
+++ resources/views/components/admin-layout.blade.php
@@ -6,14 +6,18 @@
         <title>Movie Scout</title>
         <link rel="icon" href="{{ asset("images/logomark.svg") }}" />
         @vite(["resources/css/app.css", "resources/js/app.js"])
+        <script src="//unpkg.com/alpinejs" defer></script>
     </head>
     <body class="flex">
-        <x-aside />
+        <x-aside class="sticky top-0 min-w-[300px] h-screen" />
 
         <main
-            {{ $attributes->class("md:pt-50 min-h-screen w-full max-w-5xl ") }}
+            class="flex flex-1 flex-col items-center justify-center"
+            {{ $attributes->class("md:pt-[50] min-h-screen w-full max-w-5xl ") }}
         >
             {{ $slot }}
         </main>
     </body>
 </html>
+
+{{-- "min-h-screen w-full max-w-5xl flex-1 px-4 pt-24 md:pb-16 md:pt-28") }} --}}
