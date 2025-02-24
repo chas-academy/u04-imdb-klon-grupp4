ccc9b506710eecf5b908492fd0c174666773e1f2
FredricLaestander <fredriclaestander@gmail.com>
2025-02-24 14:56:54 +0100

    re-format list-card component to fit base-card

--- resources/views/components/list-card.blade.php
+++ resources/views/components/list-card.blade.php
@@ -1,17 +1,20 @@
-@props ([
+@props([
     "title",
     "description",
     "movies" => [],
-])    
+    "href",
+])
 
-<x-base-card class="m-4 w-full h-auto">
+<x-base-card href="{{ $href }}">
     <div class="flex gap-4">
-        <x-list-cover :movies="$movies" class="w-16 h-24 object-cover" />
-    
+        <x-list-cover :movies="$movies" class="h-24 w-16 object-cover" />
+
         <div class="flex-1">
             <div class="flex justify-between">
                 <h3 class="text-lg font-bold">{{ $title }}</h3>
-                <x-lucide-x class="size-6 text-neutral-400 hover:text-neutral-100 cursor-pointer"></x-lucide-x>
+                <x-lucide-x
+                    class="size-6 cursor-pointer text-neutral-400 hover:text-neutral-100"
+                />
             </div>
             <p class="line-clamp-3 sm:line-clamp-1">{{ $description }}</p>
         </div>
