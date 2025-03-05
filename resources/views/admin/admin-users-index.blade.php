e590e738434c3da90284e8275d9355250e48dd10
SRsoz <sozyar.rauf@chasacademy.se>
2025-03-05 22:59:25 +0100

    basic rendering

---
+++ resources/views/admin/admin-users-index.blade.php
@@ -0,0 +1,13 @@
+<x-admin-layout class="mx-auto">
+<div class="mx-auto w-2/3">
+    <h1 class="text-center pt-32 pb-8 font-outfit text-2xl font-bold">
+        USER'S SETTINGS
+    </h1>
+<form class="flex flex-col gap-1">
+    <x-input-field id="name" placeholder="">Username:</x-input-field>
+    <x-input-field id="name" placeholder="">Password:</x-input-field>
+<x-input-field id="name" placeholder="">Email:</x-input-field>
+
+<div class="flex flex-row gap-1 justify-end">
+<x-button>ADD USER</x-button>
+</x-admin-layout>
