abe2f031959ccdd023d9d0e3f7fb44b099901583
SRsoz <din.email@example.comecho>
2025-03-06 16:52:26 +0100

    did changes and added form

--- resources/views/admin/admin-users-index.blade.php
+++ resources/views/admin/admin-users-index.blade.php
@@ -10,4 +10,32 @@
 
 <div class="flex flex-row gap-1 justify-end">
 <x-button>ADD USER</x-button>
+
+<div id="user-list">
+            @foreach ($users as $user)
+                <div class="relative mb-3 flex items-center justify-between">
+                    <div class="flex-grow border-b">
+                        <h2 class="inline-block font-bold">
+                            {{ $user->username}}
+                        </h2>
+                    </div>
+                    <div class="ml-4 flex gap-4">
+                        <a href="{{ route('admin.admin-users-edit', $user->username) }}">
+                            <x-lucide-pen class="size-6" />
+                        </a>
+
+                        <form
+                            action="{{ route('admin.admin-users-delete', $user->id) }}"
+                            method="POST"
+                        >
+                            @csrf
+                            @method("DELETE")
+                            <button type="submit">
+                                <x-lucide-trash class="size-6" />
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            @endforeach
+        </div>
 </x-admin-layout>
