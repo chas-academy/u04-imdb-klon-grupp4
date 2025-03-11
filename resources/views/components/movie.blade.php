4e153b5660a56a2c44709dc9f6e974b76f8739cc
FredricLaestander <fredriclaestander@gmail.com>
2025-03-11 20:40:22 +0100

    make movies clickable on every instance

--- resources/views/components/movie.blade.php
+++ resources/views/components/movie.blade.php
@@ -1,11 +1,13 @@
 @props([
+    "id",
     "title",
     "poster" => null,
     "rating" => null,
     "isBookmarked",
 ])
 
-<div
+<a
+    href="{{ route("movies.show", ["id" => $id]) }}"
     class="relative aspect-[2/3] min-w-24 cursor-pointer flex-col overflow-hidden rounded-lg"
 >
     @if (isset($poster))
@@ -17,11 +19,8 @@
     @endif
 
     <div
-        class="group relative z-20 flex size-full flex-col justify-between bg-gradient-to-t from-neutral-900 to-80% transition hover:bg-neutral-900/80"
+        class="group relative z-20 flex size-full flex-col justify-end bg-gradient-to-t from-neutral-900 to-80% transition hover:bg-neutral-900/80"
     >
-        {{-- bookmark component --}}
-        <div>bookmark</div>
-
         <div class="p-2">
             <p
                 class="line-clamp-1 text-sm font-bold group-hover:line-clamp-none md:line-clamp-2"
@@ -29,8 +28,7 @@
                 {{ $title }}
             </p>
 
-            {{-- rating-component --}}
-            <div>4.5</div>
+            <x-rating :rating="$rating" filled />
         </div>
     </div>
-</div>
+</a>
--- resources/views/genres/index.blade.php
+++ resources/views/genres/index.blade.php
@@ -2,26 +2,35 @@
     <div class="flex flex-col gap-8">
         <div class="flex flex-col">
             <div class="flex flex-col gap-1">
-                <x-previous-page href="/" page="Home"/>
+                <x-previous-page href="/" page="Home" />
                 <p class="text-2xl font-bold">Movie genres</p>
             </div>
             <p class="max-w-[45rem]">
-                Explore movie genres and find exactly what you're in the mood for.
-                Whether you're after timeless classics, hidden gems, or the latest
-                blockbusters – this is your gateway to endless entertainment!
+                Explore movie genres and find exactly what you're in the mood
+                for. Whether you're after timeless classics, hidden gems, or the
+                latest blockbusters – this is your gateway to endless
+                entertainment!
             </p>
         </div>
 
         @foreach ($genres as $genre)
             <div class="flex flex-col gap-2">
-                <x-forward-page href="{{ route('genres.show', $genre->id) }}" page="{{ ucfirst($genre->name) }}"/>
-                    <div class="grid grid-flow-col gap-4 overflow-x-auto w-full max-w-full auto-cols-[minmax(8rem,_1fr)]">
+                <x-forward-page
+                    href="{{ route('genres.show', $genre->id) }}"
+                    page="{{ ucfirst($genre->name) }}"
+                />
+                <div
+                    class="grid w-full max-w-full auto-cols-[minmax(8rem,_1fr)] grid-flow-col gap-4 overflow-x-auto"
+                >
                     @foreach ($genre->movies->take(7) as $movie)
-                        <x-movie title="{{ $movie->title }}" poster="{{ $movie->poster }}" />
+                        <x-movie
+                            id="{{ $movie->id }}"
+                            title="{{ $movie->title }}"
+                            poster="{{ $movie->poster }}"
+                        />
                     @endforeach
                 </div>
             </div>
         @endforeach
-
     </div>
 </x-main-layout>
