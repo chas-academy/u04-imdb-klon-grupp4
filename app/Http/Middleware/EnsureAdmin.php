e9a9a050be2e607343eeec296c30ace9ac3e7c5b
Mariam Aljizawi <mariam_97@live.se>
2025-03-13 03:39:19 +0100

    feat: add middleware for admin

---
+++ app/Http/Middleware/EnsureAdmin.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class EnsureAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (Auth::check() && Auth::user()->is_admin) {
+            return $next($request);
+        }
+        return redirect()->route('login.admin')->with('error', 'You do not have admin access.');
+    }
+}
